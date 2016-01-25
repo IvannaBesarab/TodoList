78598772497af470c91064fc8b2c83bec6fa59e8
foxek <ibesarab1993@gmail.com>
2016-01-25 23:46:56 +0200

    task #1

--- Native JavaScript/js/main.js
+++ Native JavaScript/js/main.js
@@ -1,34 +1,32 @@
-
 var Todo = (function(){
     const CLOSE = 'x';
+    const TASK  = 'task';
 
     function init (params) {
-        var input;
-        var list;
         var button = params.rootElement.querySelector('button');
-        button.addEventListener('click', addTask);
-
-        function addTask(event) {
-            inputText = params.rootElement.querySelector('input').value;
+        button.addEventListener('click', function(){
+            //as params.rootElement is used here, anonymous func is called.
+            //I don't know yet if its possible to pass parameters into func
+            //in case of button.addEventListener('click', addTask);
+            var inputText = params.rootElement.querySelector('input').value;
             if (!inputText){
                 return;
             }
 
-            list  = document.createElement('div');
-            list.setAttribute('class', 'task');
+            var list  = document.createElement('div');
+            list.setAttribute('class', TASK);
             list.innerHTML = inputText + '<div class="delete">'+ CLOSE +'</div>';
             list.querySelector('.delete').addEventListener('click', deleteTask);
 
             params.placeToListTasks.appendChild(list);
             params.rootElement.querySelector('input').value = '';
-
-            function deleteTask(event) {
-                this.parentNode.remove();
-            }
-
-        }
+        });
     }
 
+    function deleteTask() {
+        this.parentNode.remove();
+    }
+    
     return {
     	init : init
     };
